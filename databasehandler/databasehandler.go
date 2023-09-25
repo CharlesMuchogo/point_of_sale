a6d4b525979d53a645849cac424ade4a799e2701
charles <muchpaul2@gmail.com>
2023-09-25 23:12:39 +0300

    clean up databasehandler

--- databasehandler/databasehandler.go
+++ databasehandler/databasehandler.go
@@ -1,36 +1,8 @@
 package databasehandler
 
 import (
-	"database/sql"
-	"os"
-
-	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 	"main.go/mystructs"
 )
-
-type User struct {
-	gorm.Model
-	Username string `gorm:"size:255;not null;unique" json:"username"`
-	Password string `gorm:"size:255;not null;" json:"password"`
-}
-
-func goDotEnvVariable(key string) string {
-
-	// load .env file
-	err := godotenv.Load(".env")
-
-	CheckError(err)
-
-	return os.Getenv(key)
-}
-
-func DbConnect() *sql.DB {
-	dsn := goDotEnvVariable("DATABASEURL")
-	db, err := sql.Open("postgres", dsn)
-	CheckError(err)
-	return db
-}
 
 func AddProduct(
 	product mystructs.Product,
@@ -52,10 +24,4 @@ func GetProductCategories() []mystructs.ProductCategories {
 	var categories []mystructs.ProductCategories
 	DB.Find(&categories)
 	return categories
-}
-
-func CheckError(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
 }
